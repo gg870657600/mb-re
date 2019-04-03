eb274810a7f551037b0a75679c8ac4f8ce16596e
chenxi <870657600@qq.com>
2019-04-03 10:24:36 +0800

    恢复初始状态

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
         '/api': {
             target: 'http://192.168.1.99:8080',
@@ -51,7 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
