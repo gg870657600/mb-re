08240daa222694bf946300cd77c95811abd4e950
chenxi <870657600@qq.com>
2019-04-03 13:53:53 +0800

    element-ui style error

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+import "element-ui/lib/index.js"
 import router from './router'
 import axios from 'axios'
 // import qs from 'qs'
 import Vuex from 'vuex'
-import ElementUI from 'element-ui'
-import "element-ui/lib/theme-chalk/index.css"
-import "element-ui/lib/index.js"
+
 
 Vue.prototype.$http = axios;
 
